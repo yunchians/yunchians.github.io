1adc3d99bcac12879e4bb178acd4e2b954f72c41
chian <li_chians@yahoo.com.tw>
2019-12-23 21:04:22 +0800

    fix img url

--- API/02/js/main.js
+++ API/02/js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
                 showSearch.show();
                 // console.log(data[i].鄉鎮市區);
                 let resultContent = `<div class="col-lg-12 item">
-                    <img src="../images/noimg.jpg">
+                    <img src="../../images/noimg.jpg">
                     <div class="info">
                         <div class="name"><span class="title">景點名稱：</span>${data[i].名稱}</div>
                         <div class="address"><span class="title">地址：</span>${data[i].地址}</div>
