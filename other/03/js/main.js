684ec4c78e77c378bcb7bae05dad38237538d538
chian <li_chians@yahoo.com.tw>
2020-01-26 18:41:17 +0800

    註解整理

--- other/03/js/main.js
+++ other/03/js/main.js
@@ -1,14 +1,11 @@
-const openAward = document.querySelector('#openaward');
-const lotteryBall = document.querySelector('.ball');
-const showNum = document.querySelector('.numblock');
-const popMessage = document.querySelector('.message');
-const closeMessageBox = document.querySelector('.close');
-const confirmMessage = document.querySelector('.confirm');
+const openAward = document.querySelector('#openaward'); //開獎按鈕
+const lotteryBall = document.querySelector('.ball'); //彩球背景區塊
+const showNum = document.querySelector('.numblock'); //上方顯示中獎號碼區
+const popMessage = document.querySelector('.message'); // 訊息光箱
+const closeMessageBox = document.querySelector('.close'); // 光箱關閉按鈕
+const confirmMessage = document.querySelector('.confirm'); // 光箱確認按鈕
 
-openAward.addEventListener('click', award);
-closeMessageBox.addEventListener('click', closeMessage);
-confirmMessage.addEventListener('click', closeMessage);
-
+openAward.addEventListener('click', award); // 按鈕綁定監聽事件 click觸發
 
 function award() {
     // console.log('開獎');
@@ -22,7 +19,7 @@ function award() {
     for (i = 1; i < 46; i++) {
         arr.push(i);
         arr.sort(function() {
-            return Math.random() - 0.1;
+            return Math.random();
         })
         arr.length = 6;
     };
@@ -39,6 +36,9 @@ function award() {
         document.querySelector('.numlist').textContent = '';
     }
 }
+
+closeMessageBox.addEventListener('click', closeMessage);
+confirmMessage.addEventListener('click', closeMessage);
 
 // 關閉提示光箱
 function closeMessage() {
