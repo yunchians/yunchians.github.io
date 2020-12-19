a631f48a8c90e1a1dfa49ebbe2d0beb6dc0bada9
yunchians <li_chians@yahoo.com.tw>
2020-12-19 15:38:25 +0800

    跳轉連結設定

--- fortest/tw02/js/setting.js
+++ fortest/tw02/js/setting.js
@@ -1,9 +1,6 @@
 // QRCODE網址連結
 function openQRcodeURL() {
     const qrcodeURL = 'https://lin.ee/jlSFMN9'; // QRCODE的網址設定
-    // 沒有要跳轉的話改這樣 const qrcodeURL = '';
-    // 要跳轉就改成 const qrcodeURL = '你的網誌';
-    // 範例 const qrcodeURL = 'https://lin.ee/jlSFMN9';
     if (qrcodeURL == '') {
         return;
     } else {
@@ -13,10 +10,7 @@ function openQRcodeURL() {
 
 // 官網網址連結
 function openOfficialURL() {
-    const officialURL = ''; // 官網的網址設定
-    // 沒有要跳轉的話改這樣 const officialURL = '';
-    // 要跳轉就改成 const officialURL = '你的網誌';
-    // 範例 const officialURL = 'http://bb528.tx5588.net/';
+    const officialURL = 'http://bb538.tc88.net/'; // 官網的網址設定
     if (officialURL == '') {
         return;
     } else {
