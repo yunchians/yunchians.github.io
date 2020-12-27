7d2d91fac4a14c7a3200f6c55404d02c0ce1bb1f
yunchians <li_chians@yahoo.com.tw>
2020-12-27 13:21:25 +0800

    push tw03

---
+++ fortest/tw03/js/setting.js
@@ -0,0 +1,19 @@
+// QRCODE網址連結
+function openQRcodeURL() {
+    const qrcodeURL = 'https://lin.ee/D6Iuj0R'; // QRCODE的網址設定
+    if (qrcodeURL == '') {
+        return;
+    } else {
+        window.open(qrcodeURL);
+    }
+}
+
+// 官網網址連結
+function openOfficialURL() {
+    const officialURL = 'http://bb528.tc88.net/'; // 官網的網址設定
+    if (officialURL == '') {
+        return;
+    } else {
+        window.open(officialURL);
+    }
+}
